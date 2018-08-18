d9fe70f3edd18c69801435ac044953b24e4b19a2
sofia <sofiakinsht@gmail.com>
2018-08-18 20:05:43 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W2/04 Least Common Multiple/LCM.java
+++ algorithmic-toolbox/W2/04 Least Common Multiple/LCM.java
@@ -1,19 +1,21 @@
 import java.util.*;
 
 public class LCM {
-  private static long lcm_naive(int a, int b) {
-    for (long l = 1; l <= (long) a * b; ++l)
-      if (l % a == 0 && l % b == 0)
-        return l;
+    private static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
 
-    return (long) a * b;
-  }
+    private static long lcm(int a, int b) {
+        return (long) a * b / gcd(a, b);
+    }
 
-  public static void main(String args[]) {
-    Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
-    System.out.println(lcm_naive(a, b));
-  }
+        System.out.println(lcm(a, b));
+    }
 }
--- algorithmic-toolbox/W2/04 Least Common Multiple/lcm.py
+++ algorithmic-toolbox/W2/04 Least Common Multiple/lcm.py
@@ -1,15 +1,17 @@
-# Uses python3
+# python3
 import sys
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
 
-    return a*b
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    print(lcm(a, b))
 
