9675bbe2d007aa19c76eaf88c22875c38fd698a5
sofia <sofiakinsht@gmail.com>
2018-08-19 17:31:37 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W2/05 Fibonacci Number Again/FibonacciHuge.java
+++ algorithmic-toolbox/W2/05 Fibonacci Number Again/FibonacciHuge.java
@@ -1,27 +1,43 @@
 import java.util.*;
 
 public class FibonacciHuge {
-    private static long getFibonacciHugeNaive(long n, long m) {
-        if (n <= 1)
-            return n;
+    private static long pisanoPeriod(long mod) {
+        long prev = 0;
+        long curr = 1;
 
-        long previous = 0;
-        long current  = 1;
-
-        for (long i = 0; i < n - 1; ++i) {
-            long tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
+        long pisanoPeriod = 0;
+        for (int i = 0; i < mod * mod; i++) {
+            long next = (prev + curr) % mod;
+            prev = curr;
+            curr = next;
+            if (prev == 0 && curr == 1) {
+                pisanoPeriod = i + 1;
+                break;
+            }
         }
-
-        return current % m;
+        return pisanoPeriod;
     }
-    
+
+    private static long getFibonacciHuge(long number, long mod) {
+        long remainder = number % pisanoPeriod(mod);
+        if (remainder < 1)
+            return remainder;
+
+        long prev = 0;
+        long curr = 1;
+
+        for (int i = 1; i < remainder; i++) {
+            long next = (prev + curr) % mod;
+            prev = curr;
+            curr = next;
+        }
+        return curr % mod;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long m = scanner.nextLong();
-        System.out.println(getFibonacciHugeNaive(n, m));
+        System.out.println(getFibonacciHuge(n, m));
     }
 }
-
--- algorithmic-toolbox/W2/05 Fibonacci Number Again/fibonacci_huge.py
+++ algorithmic-toolbox/W2/05 Fibonacci Number Again/fibonacci_huge.py
@@ -1,19 +1,33 @@
-# Uses python3
+# python3
 import sys
 
-def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
+
+def get_fibonacci(n):
+    if (n < 1):
         return n
 
-    previous = 0
-    current  = 1
+    prev = 0
+    curr = 1
 
     for _ in range(n - 1):
-        previous, current = current, previous + current
+        prev, curr = curr, prev + curr
 
-    return current % m
+    return curr
+    
+def get_pisano_period(m):
+    prev = 0
+    curr = 1
+    
+    for i in range(m * m):
+        prev, curr = curr, (prev + curr) % m
+        if prev == 0 and curr == 1:
+            return i + 1
+            
+def get_fibonacci_huge(n, m):
+    remainder = n % get_pisano_period(m)
+    return get_fibonacci(remainder) % m
 
 if __name__ == '__main__':
     input = sys.stdin.read();
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_huge(n, m))
