b7323a681f155bbcb491301edc77d2164b8687db
sofia <sofiakinsht@gmail.com>
2018-08-22 09:38:45 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W3/2_maximum_value_of_the_loot/FractionalKnapsack.java
+++ algorithmic-toolbox/W3/2_maximum_value_of_the_loot/FractionalKnapsack.java
@@ -1,23 +1,56 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class FractionalKnapsack {
-    private static double getOptimalValue(int capacity, int[] values, int[] weights) {
-        double value = 0;
-        //write your code here
+    public static double getOptimalValue(int capacity, List<Item> items) {
+        Collections.sort(items);
 
-        return value;
+        double maxValue = 0;
+
+        for (Item item : items) {
+            if (capacity > 0) {
+                int fraction = Math.min(item.getWeight(), capacity);
+                maxValue += item.getValuePerWeight() * fraction;
+                capacity -= fraction;
+            }
+        }
+        return maxValue;
+    }
+
+    public static class Item implements Comparable<Item> {
+        private final int value;
+        private final int weight;
+        private final double valuePerWeight;
+
+        public Item(int value, int weight) {
+            this.value = value;
+            this.weight = weight;
+            this.valuePerWeight = (double) value / (double) weight;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public double getValuePerWeight() {
+            return valuePerWeight;
+        }
+
+        @Override
+        public int compareTo(Item o) {
+            return Double.compare(o.getValuePerWeight(), valuePerWeight);
+        }
     }
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int capacity = scanner.nextInt();
-        int[] values = new int[n];
-        int[] weights = new int[n];
+        List<Item> items = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            values[i] = scanner.nextInt();
-            weights[i] = scanner.nextInt();
+            int value = scanner.nextInt();
+            int weight = scanner.nextInt();
+            items.add(new Item(value, weight));
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+        System.out.println(getOptimalValue(capacity, items));
     }
-} 
+}
