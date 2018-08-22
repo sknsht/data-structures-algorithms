d0681b21c2b6ebecdebb8a5747d3249706643b3d
sofia <sofiakinsht@gmail.com>
2018-08-22 09:06:45 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W3/1_money_change/Change.java
+++ algorithmic-toolbox/W3/1_money_change/Change.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class Change {
-    private static int getChange(int m) {
-        //write your code here
-        return m;
+    private static int getChange(int money) {
+        int[] denominations = {10, 5, 1};
+        int minNumCoins = 0;
+
+        for (int i = 0; money > 0; i++) {
+            minNumCoins += money / denominations[i];
+            money %= denominations[i];
+        }
+        return minNumCoins;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println(getChange(m));
+        int money = scanner.nextInt();
+        System.out.println(getChange(money));
 
     }
 }
