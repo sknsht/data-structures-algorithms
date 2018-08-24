1038ed8440a704ccccb3007552def53c4f576feb
sofia <sofiakinsht@gmail.com>
2018-08-24 09:23:39 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W4/2_majority_element/MajorityElement.java
+++ algorithmic-toolbox/W4/2_majority_element/MajorityElement.java
@@ -2,14 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class MajorityElement {
-    private static int getMajorityElement(int[] a, int left, int right) {
+    private static int getMajorityElement(int[] array, int left, int right) {
         if (left == right) {
             return -1;
         }
         if (left + 1 == right) {
-            return a[left];
+            return array[left];
         }
-        //write your code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int a : array) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        int halfSize = array.length / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > halfSize) {
+                return entry.getKey();
+            }
+        }
         return -1;
     }
 
@@ -26,6 +35,7 @@ public class MajorityElement {
             System.out.println(0);
         }
     }
+
     static class FastScanner {
         BufferedReader br;
         StringTokenizer st;
@@ -54,4 +64,3 @@ public class MajorityElement {
         }
     }
 }
-
