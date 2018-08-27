8253d5d12a316eb71c03f3b679bb648059c9b2de
sofia <sofiakinsht@gmail.com>
2018-08-27 23:04:02 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W5/1_money_change_again/ChangeDP.java
+++ algorithmic-toolbox/W5/1_money_change_again/ChangeDP.java
@@ -1,16 +1,26 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChangeDP {
-    private static int getChange(int m) {
-        //write your code here
-        return m / 4;
+    private static int getMinimumChange(int m) {
+        int[] denominations = {1, 3, 4};
+
+        int[] minNumCoins = new int[m + 1];
+        Arrays.fill(minNumCoins, 1, m + 1, Integer.MAX_VALUE);
+
+        for (int i = 1; i <= m; i++) {
+            for (int d : denominations) {
+                if (i >= d) {
+                    minNumCoins[i] = Math.min(minNumCoins[i], minNumCoins[i - d] + 1);
+                }
+            }
+        }
+        return minNumCoins[m];
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println(getChange(m));
-
+        int money = scanner.nextInt();
+        System.out.println(getMinimumChange(money));
     }
 }
-
