9a1aa0d6cfde43f61e4d993badd416ab2f5e32ef
sofia <sofiakinsht@gmail.com>
2018-08-28 08:28:25 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W5/2_primitive_calculator/PrimitiveCalculator.java
+++ algorithmic-toolbox/W5/2_primitive_calculator/PrimitiveCalculator.java
@@ -1,18 +1,27 @@
 import java.util.*;
 
 public class PrimitiveCalculator {
-    private static List<Integer> optimal_sequence(int n) {
-        List<Integer> sequence = new ArrayList<Integer>();
-        while (n >= 1) {
-            sequence.add(n);
-            if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
-            }
+    private static List<Integer> optimalSequence(int n) {
+        int[] a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = a[i - 1] + 1;
+            if (i % 2 == 0)
+                a[i] = Math.min(1 + a[i / 2], a[i]);
+            if (i % 3 == 0)
+                a[i] = Math.min(1 + a[i / 3], a[i]);
         }
+
+        List<Integer> sequence = new ArrayList<>();
+        while (n > 0) {
+            sequence.add(n);
+            if (a[n - 1] == a[n] - 1)
+                n = n - 1;
+            else if ((n % 2 == 0) && (a[n / 2] == a[n] - 1))
+                n = n / 2;
+            else if ((n % 3 == 0) && (a[n / 3] == a[n] - 1))
+                n = n / 3;
+        }
+
         Collections.reverse(sequence);
         return sequence;
     }
@@ -20,11 +29,10 @@ public class PrimitiveCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        List<Integer> sequence = optimal_sequence(n);
+        List<Integer> sequence = optimalSequence(n);
         System.out.println(sequence.size() - 1);
-        for (Integer x : sequence) {
-            System.out.print(x + " ");
+        for (int s : sequence) {
+            System.out.print(s + " ");
         }
     }
 }
-
