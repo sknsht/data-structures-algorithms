30b5c971eda647b3c5e256bdd571f902c03d079f
sofia <sofiakinsht@gmail.com>
2018-08-29 08:45:25 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W5/03 Edit Distance/EditDistance.java
+++ algorithmic-toolbox/W5/03 Edit Distance/EditDistance.java
@@ -1,17 +1,35 @@
 import java.util.*;
 
 class EditDistance {
-  public static int EditDistance(String s, String t) {
-    //write your code here
-    return 0;
-  }
-  public static void main(String args[]) {
-    Scanner scan = new Scanner(System.in);
+    public static int EditDistance(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] alignment = new int[n + 1][m + 1];
 
-    String s = scan.next();
-    String t = scan.next();
+        for (int j = 1; j <= m; j++) {
+            alignment[0][j] = j;
+        }
+        for (int i = 1; i <= n; i++) {
+            alignment[i][0] = i;
+        }
 
-    System.out.println(EditDistance(s, t));
-  }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int insertion = alignment[i][j - 1] + 1;
+                int deletion = alignment[i - 1][j] + 1;
+                int substitution = alignment[i - 1][j - 1] + (s.charAt(i - 1) == t.charAt(j - 1) ? 0 : 1);
+                alignment[i][j] = Math.min(insertion, Math.min(deletion, substitution));
+            }
+        }
+        return alignment[n][m];
+    }
 
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.next();
+        String t = scan.next();
+
+        System.out.println(EditDistance(s, t));
+    }
 }
