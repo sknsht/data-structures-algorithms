e0e6b4ac376593d6423565ab26e907553cffeec6
sofia <sofiakinsht@gmail.com>
2018-08-30 10:51:36 +0200

    Algorithmic Toolbox Course

--- algorithmic-toolbox/W6/02 Partitioning Souvenirs/Partition3.java
+++ algorithmic-toolbox/W6/02 Partitioning Souvenirs/Partition3.java
@@ -1,10 +1,29 @@
 import java.util.*;
-import java.io.*;
 
 public class Partition3 {
     private static int partition3(int[] A) {
-        //write your code here
-        return 0;
+        int sum = Arrays.stream(A).sum();
+        if (sum == 0 || sum % 3 != 0)
+            return 0;
+
+        int n = A.length;
+        int[][] tab = new int[sum / 3 + 1][n + 1];
+
+        for (int i = 1; i <= sum / 3; i++) {
+            for (int j = 1; j <= n; j++) {
+                int diff = i - A[j - 1];
+                if ((A[j - 1] == i) || (diff > 0 && tab[diff][j - 1] > 0)) {
+                    tab[i][j] = (tab[i][j - 1] == 0) ? 1 : 2;
+                } else {
+                    tab[i][j] = tab[i][j - 1];
+                }
+            }
+        }
+
+        if (tab[sum / 3][n] == 2)
+            return 1;
+        else
+            return 0;
     }
 
     public static void main(String[] args) {
@@ -17,4 +36,3 @@ public class Partition3 {
         System.out.println(partition3(A));
     }
 }
-
